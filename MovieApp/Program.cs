343b26018c796b0118c47619bed397fce10dcbbd
Raluca Miclea <rmiclea02@gmail.com>
2018-11-08 09:37:30 +0200

    added config file for MovieApp

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using HomemadeIoCController;
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 
 namespace MovieApp
 {
@@ -7,16 +9,27 @@ namespace MovieApp
     {
         public static void Main()
         {
+            var config = GetConfig("config");
             IIoCContainer container = new IoCContainer();
+            container.Config(config, typeof(Program).Assembly);
 
-            container.RegisterTransientType<IMovieFinder, MovieFinderTxt>();
+            //container.RegisterTransientType<IMovieFinder, MovieFinderTxt>();
             //container.RegisterTransientType<IMovieFinder, MovieFinderJson>();
-            container.RegisterSingletonType<IMovieLister, MovieLister>();
+            //container.RegisterSingletonType<IMovieLister, MovieLister>();
 
             var obj = container.Resolve<IMovieLister>();
             obj.ListMoviesFromDirector("Director1");
 
             Console.ReadKey();
+        }
+
+        public static JObject GetConfig(string taxReturnName)
+        {
+            using (StreamReader r = new StreamReader($"{taxReturnName}.json"))
+            {
+                string json = r.ReadToEnd();
+                return JObject.Parse(json);
+            }
         }
     }
 }
